48f5777b15e9b444db4167439d85f524bf4b9e6f
morganm0909 <104795413+morganm0909@users.noreply.github.com>
2023-10-05 01:55:16 -0400

    Update timer.c

--- signals/timer.c
+++ signals/timer.c
@@ -6,32 +6,40 @@
 #include <stdbool.h>
 #include <time.h>
 
-time_t stop, start;
-bool a = false;
+int handled = 0;
+int alarm_count = 0;
+int t = 1;
 
 void handler(int signum)
 { //signal handler
-  stop = time(NULL);
   printf("Hello World!\n");
-  a = true;
+  handled = 1;
   //exit(1); //exit after printing
 }
 
 void handler2(int signum)
 { //signal handler
-  printf("Hello World!\n");
+  printf("\nAlarms: %d\n", alarm_count);
+  printf("Total time executed: %d\n", alarm_count);
+  t = 0;
   //exit(1); //exit after printing
 }
 
 int main(int argc, char * argv[])
 {
   signal(SIGALRM,handler); //register handler to handle SIGALRM
-  signal(SIGALRM,handler2);
-  alarm(1); //Schedule a SIGALRM for 1 second
-  start = time(NULL);
-  while(a == false){
-    printf("Turing was right!\n");
-    sleep(1)
+  signal(SIGINT,handler2);
+
+  while(t){
+    alarm(1); //Schedule a SIGALRM for 1 second
+    pause();
+    if (1){
+      printf("Turing was right!\n");
+      handled = 0;
+      alarm_count = alarm_count + 1;
+    }
+    // printf("Turing was right!\n");
+    // sleep(1);
   }; //busy wait for signal to be delivered
   //exit(1);
   return 0; //never reached
